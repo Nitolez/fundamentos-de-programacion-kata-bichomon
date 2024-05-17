510d7fda7d6b25244b129e0a41e5e2b3b92cc197
Nitolez <165071237+Nitolez@users.noreply.github.com>
2024-05-17 11:15:24 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,31 @@
-// Cambia el título "Generation 1 Pokémon" por "Generasión 1 Pokimon"
+// Cambia el título 
     const titulo = document.querySelector('h1');
     if (titulo) {
         titulo.textContent = 'Generasión 1 Pokimon';
     }
 
-    // Cambia el color de fondo de la primera generación de Pokimon
+    // Cambia el color de fondo de la primera generación
     const primGen = document.querySelector('.infocard');
     if (primGen) {
         primGen.style.backgroundColor = 'yellow'
     }
 
-    // Imprime por consola la URL de la página
+    //URL de la página
     console.log('URL de la página:', window.location.href);
 
-    // Imprime por consola el dominio de la página
+    //Dominio de la página
     console.log('Dominio de la página:', window.location.hostname);
 
-    // Imprime todos los nodos de imagen
+    // Nodos de imagen
     const imagenes = document.querySelectorAll('img');
     console.log(imagenes);
 
-    // Sustituye el atributo "src" de todas las imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+    // Sustituye el atributo "src" 
     imagenes.forEach(img => {
         img.src = 'https://media.giphy.com/media/2v170e71aanfi/giphy.gif';
     });
 
-    // Cambia el fondo de todos los infocard-lg-data text-muted para todos los Pokimon voladores itype flying
+    // Cambia el fondo de los Pokemon voladores
     const voladores = document.querySelectorAll('.infocard-lg-data.text-muted .itype.flying');
     voladores.forEach(vol => {
         const infocard = vol.closest('.infocard-lg-data.text-muted');
@@ -33,4 +33,3 @@
             infocard.style.backgroundColor = 'blue'
         }
     });
-
